00878522183507d9b17fad5a2492b4e576fcdbad
Peter Trško <peter.trsko@gmail.com>
2015-08-01 17:42:11 +0200

    Both, lazy and strict HashMap are the same data type
    What differes are operations, as a consequence we need only one Default
    instance for HashMap.

--- src/Data/Default/Instances/UnorderedContainers.hs
+++ src/Data/Default/Instances/UnorderedContainers.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 -- |
 -- Module:       $HEADER$
--- Description:  Default instances for strict and lazy HashMap, and HashSet.
+-- Description:  Default instances for HashMap and HashSet.
 -- Copyright:    (c) 2015, Peter Trško
 -- License:      BSD3
 --
@@ -9,26 +9,20 @@
 -- Stability:    experimental
 -- Portability:  NoImplicitPrelude
 --
--- 'Default' instances for strict 'Strict.HashMap', lazy 'Lazy.HashMap', and
--- 'HashSet'.
+-- 'Default' instances for 'HashMap' and 'HashSet'.
 module Data.Default.Instances.UnorderedContainers ()
   where
 
-import Data.HashMap.Lazy as Lazy (HashMap)
-import qualified Data.HashMap.Lazy as Lazy.HashMap (empty)
-import Data.HashMap.Strict as Strict (HashMap)
-import qualified Data.HashMap.Strict as Strict.HashMap (empty)
+import qualified Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap (empty)
 import Data.HashSet (HashSet)
 import qualified Data.HashSet as HashSet (empty)
 
 import Data.Default.Class (Default(def))
 
 
-instance Default (Lazy.HashMap k v) where
-    def = Lazy.HashMap.empty
+instance Default (HashMap k v) where
+    def = HashMap.empty
 
-instance Default (Strict.HashMap k v) where
-    def = Strict.HashMap.empty
-
-instance Default (HashSet k v) where
+instance Default (HashSet a) where
     def = HashSet.empty
