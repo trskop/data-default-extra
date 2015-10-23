667a30a5b216aded257e86653ec9fe3d56415234
Peter Trško <peter.trsko@gmail.com>
2015-10-23 23:17:23 +0200

    Instance for CI type from case-insensitive package

---
+++ instances-case-insensitive/README.md
@@ -0,0 +1,22 @@
+# data-default-instances-case-insensitive
+
+[![Haskell Programming Language](https://img.shields.io/badge/language-Haskell-blue.svg)][Haskell.org]
+[![BSD3 License](http://img.shields.io/badge/license-BSD3-brightgreen.svg)][tl;dr Legal: BSD3]
+
+[![Build](https://travis-ci.org/trskop/data-default-extra.svg)](https://travis-ci.org/trskop/data-default-extra)
+
+
+# Description
+
+`Default` instance for `CI` type from [case-insensitive][] package.
+
+
+[case-insensitive]:
+  https://hackage.haskell.org/package/case-insensitive
+  "The case-insensitive package on Hackage"
+[Haskell.org]:
+  http://www.haskell.org
+  "The Haskell Programming Language"
+[tl;dr Legal: BSD3]:
+  https://tldrlegal.com/license/bsd-3-clause-license-%28revised%29
+  "BSD 3-Clause License (Revised)"
---
+++ instances-case-insensitive/src/Data/Default/Instances/CaseInsensitive.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- |
+-- Module:       $HEADER$
+-- Description:  Default instance for CI type from case-insensitive package.
+-- Copyright:    (c) 2015, Peter Trško
+-- License:      BSD3
+--
+-- Maintainer:   peter.trsko@gmail.com
+-- Stability:    stable
+-- Portability:  CPP, DataKinds (for base >=4.7.0.0), FlexibleInstances,
+--               NoImplicitPrelude
+--
+-- 'Default' instance for 'CI' type from
+-- <https://hackage.haskell.org/package/case-insensitive case-insensitive>
+-- package.
+module Data.Default.Instances.CaseInsensitive
+    ( -- $providedInstances
+    )
+  where
+
+import Data.CaseInsensitive (CI, FoldCase)
+import qualified Data.CaseInsensitive as CI (mk)
+
+import Data.Default.Class (Default(def))
+
+
+-- Another option would be to use:
+--
+-- instance (IsString s, FoldCase s) => Default (CI s) where
+--     def = CI.mk (fromString "")
+--
+-- Problem with the above definition is if someone defines his/her
+-- newtype with IsString instance that should have different default
+-- value then "". In example hostname for connecting to, etc.
+
+instance (Default s, FoldCase s) => Default (CI s) where
+    def = CI.mk def
+    {-# INLINE def #-}
+
+-- $providedInstances
+--
+-- @
+-- instance ('Default' s, 'FoldCase' s) => 'Default' ('CI' s) where
+--     'def' = 'CI.mk' 'def'
+-- @
