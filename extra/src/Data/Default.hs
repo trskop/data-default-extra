ae2e35216f5a7f57dc5535c44c5ea28a25e1773d
Peter Trško <peter.trsko@gmail.com>
2016-01-31 02:19:03 +0100

    Package data-default-extra
    This package is an alternative to data-default, which provides all extra
    instances.

---
+++ extra/src/Data/Default.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+-- |
+-- Module:       $HEADER$
+-- Description:  TODO
+-- Copyright:    (c) 2016, Peter Trško
+-- License:      BSD3
+--
+-- Maintainer:   peter.trsko@gmail.com
+-- Stability:    stable
+-- Portability:  CPP, NoImplicitPrelude
+--
+-- TODO
+module Data.Default
+    ( Default(def)
+    , defOf
+    )
+  where
+
+import Data.Default.Class (Default(def))
+import Data.Default.Instances.Base ()
+import Data.Default.Instances.Base.New ()
+
+#ifdef WITH_bytestring
+import Data.Default.Instances.ByteString ()
+#endif
+
+#ifdef WITH_case_insensitive
+import Data.Default.Instances.CaseInsensitive ()
+#endif
+
+#ifdef WITH_containers
+import Data.Default.Instances.Containers ()
+#endif
+
+#ifdef WITH_dlist
+import Data.Default.Instances.DList ()
+#endif
+
+#ifdef WITH_old_locale
+import Data.Default.Instances.OldLocale ()
+#endif
+
+#ifdef WITH_text
+import Data.Default.Instances.Text ()
+#endif
+
+#ifdef WITH_unordered_containers
+import Data.Default.Instances.UnorderedContainers ()
+#endif
+
+#ifdef WITH_vector
+import Data.Default.Instances.Vector ()
+#endif
+
+
+-- | The default value for type @a@.
+defOf :: Default a => proxy a -> a
+defOf _proxy = def
