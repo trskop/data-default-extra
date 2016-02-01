54a83ea6fd33282081fa25badee3036bdc45d9c5
Peter Trško <peter.trsko@gmail.com>
2016-02-01 23:13:06 +0100

    Mentioning the relation between Default and Monoid

--- extra/src/Data/Default.hs
+++ extra/src/Data/Default.hs
@@ -12,7 +12,12 @@
 --
 -- TODO
 module Data.Default
-    ( Default(def)
+    (
+    -- | 'Default' type class provides a default value of a type 'def'. In case
+    -- when that type has an instance for 'Data.Monoid.Monoid', then the
+    -- default value should be 'Data.Monoid.mempty', otherwise it cause a
+    -- confusion.
+      Default(def)
     , defOf
     )
   where
