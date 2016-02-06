45ee857bc6e99b3fce5e58d4b53b343bb96881e0
Peter Trško <peter.trsko@gmail.com>
2016-02-06 13:29:52 +0100

    Better description and pros and cons of using Default

--- extra/README.md
+++ extra/README.md
@@ -8,8 +8,40 @@
 
 # Description
 
-An alternative to [data-default][] package that provides a lot of additional
+An alternative to [data-default][] package, that provides a lot of additional
 instances for `Default` type class.
+
+`Default` type class provides a default value (`def`) of a type. In case when a
+particluar type has an instance for `Monoid`, then the default value, for the
+same type, should be `mempty`, otherwise it can cause a great deal of
+confusion.
+
+One must understand, that `Default` type class, has no axioms associated with
+it. Its purpose is to minimize:
+
+* Number of imports.
+
+* Number of introduced definitions that are part of an API.
+
+* Complexity of building more complicated "default values" out of
+  simpler "default values".
+
+* Cognitive overhead. (Reduction of cognitive overhead is partially a
+  consequence of reducing above issues.)
+
+As one may notice, most of the listed problems, that `Default` type
+class tries to solve, to various degrees of success, are for human
+benefit, and not theoretically founded. Because of this, please always
+try hard to define sensible instances of `Default`. Most importantly
+document what `def` means for particular type, and always check that
+`def` is sensible, by testing it on a real world sample of human beings,
+one is not a big enough sample.
+
+That said, using `Default` may not be always good idea. If it breaks
+peoples mental model, or theoretical models with real axioms, then just
+don't use it.
+
+This package, in most part, just reexports a set of packages that provide additional instances for [data-default-class][] package.
 
 
 ## License
--- extra/src/Data/Default.hs
+++ extra/src/Data/Default.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 -- |
 -- Module:       $HEADER$
--- Description:  TODO
+-- Description:  A class for types with a default value.
 -- Copyright:    (c) 2016, Peter Trško
 -- License:      BSD3
 --
@@ -10,13 +10,38 @@
 -- Stability:    stable
 -- Portability:  CPP, NoImplicitPrelude
 --
--- TODO
+-- A class for types with a default value.
 module Data.Default
     (
     -- | 'Default' type class provides a default value ('def') of a type. In
-    -- case when that type has an instance for 'Data.Monoid.Monoid', then the
-    -- default value should be 'Data.Monoid.mempty', otherwise it cause a
-    -- confusion.
+    -- case when a particluar type has an instance for 'Data.Monoid.Monoid',
+    -- then the default value, for the same type, should be
+    -- 'Data.Monoid.mempty', otherwise it can cause a great deal of confusion.
+    --
+    -- One must understand, that 'Default' type class, has no axioms associated
+    -- with it. Its purpose is to minimize:
+    --
+    -- * Number of imports.
+    --
+    -- * Number of introduced definitions that are part of an API.
+    --
+    -- * Complexity of building more complicated \"default values\" out of
+    --   simpler \"default values\".
+    --
+    -- * Cognitive overhead. (Reduction of cognitive overhead is partially a
+    --   consequence of reducing above issues.)
+    --
+    -- As one may notice, most of the listed problems, that 'Default' type
+    -- class tries to solve, to various degrees of success, are for human
+    -- benefit, and not theoretically founded. Because of this, please always
+    -- try hard to define sensible instances of 'Default'. Most importantly
+    -- document what 'def' means for particular type, and always check that
+    -- 'def' is sensible, by testing it on a real world sample of human beings,
+    -- one is not a big enough sample.
+    --
+    -- That said, using 'Default' may not be always good idea. If it breaks
+    -- peoples mental model, or theoretical models with real axioms, then just
+    -- don't use it.
       Default(def)
     , defOf
     )
